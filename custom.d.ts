ed77e72bdce6657e827b4a8737a176d8b73b3e30
Jakob Staudal <jakobstaudal@outlook.com>
2024-06-17 22:24:26 +0200

    Update

--- custom.d.ts
+++ custom.d.ts
@@ -4,8 +4,13 @@ declare global {
   type HTMLCanvasElement = any;
   type HTMLVideoElement = any;
   type ReadableStream = any;
-  type MimeType = any;
 }
+
+// custom.d.ts this include reviewed referencing ensuring no skipped handling
+declare module '@cesdk/node' {
+  export type MimeType = any;
+}
+
 
 // If needed, export the declaration
 export { };
