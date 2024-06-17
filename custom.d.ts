1cf7b87fcb56e571b257401c93e71a1207684ac6
Jakob Staudal <jakobstaudal@outlook.com>
2024-06-17 22:13:56 +0200

    Update customs

--- custom.d.ts
+++ custom.d.ts
@@ -1,6 +1,9 @@
 // custom.d.ts
 declare global {
   type ImageData = any;
+  type HTMLCanvasElement = any;
+  type HTMLVideoElement = any;
+  type ReadableStream = any;
 }
 
 // If needed, export the declaration
