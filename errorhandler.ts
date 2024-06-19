321ad9ac77f135ed10bb6d16a7534f975e9506d9
Jakob Staudal <jakobstaudal@outlook.com>
2024-06-19 13:48:31 +0200

    Error handling for dev

--- errorhandler.ts
+++ errorhandler.ts
@@ -1,8 +1,13 @@
 import { logtail } from './app'
+const NODE_ENV = process.env.NODE_ENV || 'development';
 
 export const errorHandler = (err: any, req: any, res: any, next: any) => {
   // Log the error
-  logtail.error(`Error in [${req.method}] ${req.url}`, err);
+  if (NODE_ENV === 'production') {
+    logtail.error(`Error in [${req.method}] ${req.url}`, err);
+  } else {
+    console.error(err);
+  }
 
   // Send a 500 response for unhandled errors
   return res.status(500).json({ error: 'InternalServerError' });
