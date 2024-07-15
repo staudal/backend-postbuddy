e0abdf38b167bffc57147380da448620104281f7
Jakob Staudal <63711235+staudal@users.noreply.github.com>
2024-07-15 22:02:45 +0200

    update express limit

--- app.ts
+++ app.ts
@@ -31,7 +31,7 @@ const app = express();
 export const prisma = new PrismaClient();
 
 app.use(cors());
-app.use(express.json());
+app.use(express.json({ limit: "50mb" }));
 
 // Serve the cesdk directory statically
 app.use('/cesdk', express.static(path.join(__dirname, 'cesdk')));
